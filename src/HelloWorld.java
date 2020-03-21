21780e74080c69b946c99c33724eebf2c451146e
User <User@DESKTOP-UDGP4DE>
2020-03-21 12:59:27 +0900

    Creat a Menu

---
+++ src/HelloWorld.java
@@ -0,0 +1,16 @@
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("--- Bank Management System ---");
+		System.out.println(" 1. Add Bank");
+		System.out.println(" 2. Delete Bank");
+		System.out.println(" 3. Edit Bank");
+		System.out.println(" 4. View Bank");
+		System.out.println(" 5. Exit");
+		System.out.println("Select one number between 1 - 5");
+		
+	}
+
+}
